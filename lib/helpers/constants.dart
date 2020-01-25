123d305dcd08cbc958ec6b5911d06d399623c187
OStillman <owenstillman@live.co.uk>
2020-01-25 16:01:45 +0000

    Home Screen coming together

---
+++ lib/helpers/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+//Colours
+Color colourMain = Color.fromRGBO(53,135,164, 1.0);
+Color colorBody = Color.fromRGBO(45,137,139, 1.0);
+Color colorButtons = Color.fromRGBO(73,87,89, 1.0);
